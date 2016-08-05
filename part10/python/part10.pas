6f74208e153bdbdf6bf9f1c3d3c3496858c0f033
Ruslan Spivak <ruslan.spivak@gmail.com>
2016-08-04 21:08:03 -0400

    Part 10

---
+++ part10/python/part10.pas
@@ -0,0 +1,22 @@
+PROGRAM Part10;
+VAR
+   number     : INTEGER;
+   a, b, c, x : INTEGER;
+   y          : REAL;
+
+BEGIN {Part10}
+   BEGIN
+      number := 2;
+      a := number;
+      b := 10 * a + 10 * number DIV 4;
+      c := a - - b
+   END;
+   x := 11;
+   y := 20 / 7 + 3.14;
+   { writeln('a = ', a); }
+   { writeln('b = ', b); }
+   { writeln('c = ', c); }
+   { writeln('number = ', number); }
+   { writeln('x = ', x); }
+   { writeln('y = ', y); }
+END.  {Part10}
