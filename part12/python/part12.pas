64b76750dce9b48698964f30b53974f48d19bd47
Ruslan Spivak <rspivak@oanda.com>
2016-11-27 14:24:11 -0500

    Part 12. Nested procedures.

---
+++ part12/python/part12.pas
@@ -0,0 +1,23 @@
+PROGRAM Part12;
+VAR
+   a : INTEGER;
+
+PROCEDURE P1;
+VAR
+   a : REAL;
+   k : INTEGER;
+
+   PROCEDURE P2;
+   VAR
+      a, z : INTEGER;
+   BEGIN {P2}
+      z := 777;
+   END;  {P2}
+
+BEGIN {P1}
+
+END;  {P1}
+
+BEGIN {Part12}
+   a := 10;
+END.  {Part12}
